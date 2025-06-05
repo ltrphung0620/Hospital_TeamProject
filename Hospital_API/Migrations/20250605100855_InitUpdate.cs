0ac6f96a8d183c6c1921d8db7e772d1cb2e26c3e
Nghia Huynh Trung <trungnghia.huynh@hsnservice.com>
2025-06-05 17:21:32 +0700

    AddGitIgnore

---
+++ Hospital_API/Migrations/20250605100855_InitUpdate.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hospital_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermission_Permission_PermissionId",
+                table: "RolePermission");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermission_Roles_RoleId",
+                table: "RolePermission");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RolePermission",
+                table: "RolePermission");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Permission",
+                table: "Permission");
+
+            migrationBuilder.RenameTable(
+                name: "RolePermission",
+                newName: "RolePermissions");
+
+            migrationBuilder.RenameTable(
+                name: "Permission",
+                newName: "Permissions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RolePermission_PermissionId",
+                table: "RolePermissions",
+                newName: "IX_RolePermissions_PermissionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RolePermissions",
+                table: "RolePermissions",
+                columns: new[] { "RoleId", "PermissionId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Permissions",
+                table: "Permissions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermissions_Permissions_PermissionId",
+                table: "RolePermissions",
+                column: "PermissionId",
+                principalTable: "Permissions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermissions_Roles_RoleId",
+                table: "RolePermissions",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermissions_Permissions_PermissionId",
+                table: "RolePermissions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RolePermissions_Roles_RoleId",
+                table: "RolePermissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RolePermissions",
+                table: "RolePermissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Permissions",
+                table: "Permissions");
+
+            migrationBuilder.RenameTable(
+                name: "RolePermissions",
+                newName: "RolePermission");
+
+            migrationBuilder.RenameTable(
+                name: "Permissions",
+                newName: "Permission");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RolePermissions_PermissionId",
+                table: "RolePermission",
+                newName: "IX_RolePermission_PermissionId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RolePermission",
+                table: "RolePermission",
+                columns: new[] { "RoleId", "PermissionId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Permission",
+                table: "Permission",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermission_Permission_PermissionId",
+                table: "RolePermission",
+                column: "PermissionId",
+                principalTable: "Permission",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RolePermission_Roles_RoleId",
+                table: "RolePermission",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
