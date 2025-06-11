f0db3c64a21170446ce367228363dd4ca6778665
ltrphung0620 <24520593@gm.uit.edu.vn>
2025-06-11 20:13:30 +0700

    Update11_6 HospitalDbContext.cs

--- Hospital_API/Data/HospitalDbContext.cs
+++ Hospital_API/Data/HospitalDbContext.cs
@@ -1,20 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using Hospital_API.Models;
+
 namespace Hospital_API.Data
 {
     public class HospitalDbContext : DbContext
     {
-        //contructor
         public HospitalDbContext(DbContextOptions<HospitalDbContext> options) : base(options) { }
-        // add các Model entity
-        public DbSet<Medicines> Medicines { get; set; }
-        public DbSet<MedicineSupplier> MedicineSuppliers { get; set; }
-        public DbSet<MedicalServiceDb> MedicalServices { get; set; }
+
         public DbSet<Branch> Branches { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Lab_test> LabTests { get; set; }
+        public DbSet<MedicalRecord> MedicalRecords { get; set; }
+        public DbSet<MedicalServiceDb> MedicalServices { get; set; }
+        public DbSet<MedicineSupplier> MedicineSuppliers { get; set; }
+        public DbSet<Medicines> Medicines { get; set; }
         public DbSet<Permission> Permissions { get; set; }
-        public DbSet<LabTest> LabTests { get; set; }
-        // public DbSet<Role_Permission> Role_Permissions { get; set; }
+        public DbSet<PrescriptionDetails> PrescriptionDetails { get; set; }
+        public DbSet<Prescriptions> Prescriptions { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Role_Permissions> Role_Permissions { get; set; }
+        public DbSet<TestRequest> TestRequests { get; set; }
+        public DbSet<TestResult> TestResults { get; set; }
+        public DbSet<WaitingList> WaitingLists { get; set; }
     }
 }
