ea5857a4bca36d589abea5cc7d9d63fc8dbf401d
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2025-06-22 22:20:31 +0700

    Update code Admin page

--- hospital-react-app/src/App.jsx
+++ hospital-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
 import ServicesPage from "./pages/ServicesPage";
 import DepartmentsPage from "./pages/DepartmentsPage";
 import BookingPage from "./pages/BookingPage";
@@ -10,27 +10,48 @@ import RegisterPage from "./pages/RegisterPage";
 import BlogPage from "./pages/BlogPage";
 import BlogSinglePage from "./pages/BlogSinglePage";
 import ContactPage from "./pages/ContactPage";
+import TeamPage from "./pages/TeamPage";
 import ReviewPage from "./pages/ReviewPage";
+import PricingPage from "./pages/PricingPage";
 import GalleryPage from "./pages/GalleryPage";
+
+// Admin pages
+import AdminLayout from './components/admin/AdminLayout';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import UserManagementPage from './pages/admin/UserManagementPage';
+import AppointmentManagementPage from './pages/admin/AppointmentManagementPage';
+import MedicineManagementPage from './pages/admin/MedicineManagementPage';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/departments" element={<DepartmentsPage />} />
-        <Route path="/booking" element={<BookingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:id" element={<BlogSinglePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/review" element={<ReviewPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      {/* Frontend Routes */}
+      <Route path="/" element={<Layout><Home /></Layout>} />
+      <Route path="/about" element={<Layout><About /></Layout>} />
+      <Route path="/services" element={<Layout><ServicesPage /></Layout>} />
+      <Route path="/departments" element={<Layout><DepartmentsPage /></Layout>} />
+      <Route path="/booking" element={<Layout><BookingPage /></Layout>} />
+      <Route path="/blog" element={<Layout><BlogPage /></Layout>} />
+      <Route path="/blog/:id" element={<Layout><BlogSinglePage /></Layout>} />
+      <Route path="/contact" element={<Layout><ContactPage /></Layout>} />
+      <Route path="/team" element={<Layout><TeamPage /></Layout>} />
+      <Route path="/review" element={<Layout><ReviewPage /></Layout>} />
+      <Route path="/pricing" element={<Layout><PricingPage /></Layout>} />
+      <Route path="/gallery"element={<Layout><GalleryPage/></Layout>}/>
+
+      {/* Auth routes that shouldn't have the main layout */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<AdminDashboard />} />
+        <Route path="dashboard" element={<AdminDashboard />} />
+        <Route path="users" element={<UserManagementPage />} />
+        <Route path="appointments" element={<AppointmentManagementPage />} />
+        <Route path="medicines" element={<MedicineManagementPage />} />
+      </Route>
+    </Routes>
   );
 }
 
